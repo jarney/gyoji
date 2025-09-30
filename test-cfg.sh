3f0418f3985c9ffe444c99ecb84f160f7d07f121
Jon Arney <jarneyg@gmail.com>
2025-09-30 16:05:23 -0700

    Added DOT generation to tests so we can be sure we're not regressing on code-generation.

--- test-cfg.sh
+++ test-cfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #set -e
-set -x
+#set -x
 
 # For testing control-flow constructs:
 # This test builds the control-flow graph
@@ -25,6 +25,7 @@ TEST_FILES="semantics-if-else"
 
 echo "Checking token stream output."
 let failed=0
+failed_message=""
 for TEST_FILE in ${TEST_FILES} ; do
     echo -n "    ${test_file}"
 
@@ -35,27 +36,36 @@ for TEST_FILE in ${TEST_FILES} ; do
 	${CMAKE_SOURCE_DIR}/tests/${TEST_FILE}.j \
 	${TEST_CFG_DIR}/${TEST_FILE}.o
     if [ $? -ne 0 ] ; then
-	echo "Failure in compile stage"
+	failed_message="Failure failed compiling object file ${TEST_CFG_DIR}/${TEST_FILE}.o"
         failed=1
     fi
     
     # 2. Process .ll into a dot call-graph (intermediate)
     opt-18 --passes=dot-cfg --cfg-dot-filename-prefix=${TEST_CFG_DIR}/${TEST_FILE} ${TEST_CFG_DIR}/${TEST_FILE}.o.ll
     if [ $? -ne 0 ] ; then
-	echo "Failure in dot stage"
+	failed_message="Failure generating dot graph ${TEST_CFG_DIR}/${TEST_FILE}.o.ll"
 	failed=1
     fi
 
     # 3. Process DOT files (one for each function in translation unit)
     # into call graph PNG files for inspection.
-    for file in $(ls ${TEST_CFG_DIR}/${TEST_FILE}*) ; do
+    for file in $(ls ${TEST_CFG_DIR}/${TEST_FILE}*.dot) ; do
 	dot ${file} -Tpng -o ${file}.png
+	if [ $? -ne 0 ] ; then
+	    failed_message="Failed generating dot file ${file}.png"
+	    failed=1
+	fi
     done
 
     if [ ${failed} -ne 0 ] ; then
-        echo " : FAILED"
+        echo " : FAILED ${failed_message}"
     else
 	echo " : PASSED"
     fi
 
 done
+
+if [ ${failed} -ne 0 ] ; then
+    exit 1
+fi
+exit 0
